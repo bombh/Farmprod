20719e438839f65e170824a6ce4492780936cd40
bombh <fabrice@bxxl.be>
2024-03-02 23:39:51 +0100

    Add nativewind

--- app/index.jsx
+++ app/index.jsx
@@ -2,33 +2,10 @@ import { StyleSheet, Text, View } from "react-native";
 
 export default function Page() {
   return (
-    <View style={styles.container}>
-      <View style={styles.main}>
-        <Text style={styles.title}>Hello World</Text>
-        <Text style={styles.subtitle}>This is the first page of your app.</Text>
-      </View>
+    <View className="flex-1 items-center justify-center p-5 bg-black">
+        <Text className="text-white text-6xl">Hello World</Text>
+        <Text className="text-white text-lg">This is the first page of your app.</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    padding: 24,
-  },
-  main: {
-    flex: 1,
-    justifyContent: "center",
-    maxWidth: 960,
-    marginHorizontal: "auto",
-  },
-  title: {
-    fontSize: 64,
-    fontWeight: "bold",
-  },
-  subtitle: {
-    fontSize: 36,
-    color: "#38434D",
-  },
-});

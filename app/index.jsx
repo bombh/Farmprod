5bc2ea062936b699feb08e8b8bde3c04447a50f0
bombh <fabrice@bxxl.be>
2024-03-05 13:39:36 +0100

    Splash screen

--- app/index.jsx
+++ app/index.jsx
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function Page() {
   return (
     <View className="flex-1 items-center justify-center p-5 bg-black">
-        <Text className="text-white text-6xl">Hello World</Text>
+        <Text className="text-white text-5xl">Farmprod's Tour</Text>
         <Text className="text-white text-lg">This is the first page of your app.</Text>
     </View>
   );

d808fb58687525583301d5987c5f3c70d1100a9c
bombh <fabrice@bxxl.be>
2024-03-07 20:03:10 +0100

    Invert icons & splash

--- app/index.jsx
+++ app/index.jsx
@@ -2,9 +2,9 @@ import { StyleSheet, Text, View } from "react-native";
 
 export default function Page() {
   return (
-    <View className="flex-1 items-center justify-center p-5 bg-black">
-        <Text className="text-white text-5xl">Farmprod's Tour</Text>
-        <Text className="text-white text-lg">This is the first page of your app.</Text>
+    <View className="flex-1 items-center justify-center p-5 bg-white">
+        <Text className="text-black text-4xl">Farmprod's Tours</Text>
+        <Text className="text-black">This is the first page of your app.</Text>
     </View>
   );
 }
